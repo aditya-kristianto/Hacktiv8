fafdb0727ce1de8c1169039554b84214ce8cba59
Aditya Kristianto <kristianto.aditya@gmail.com>
2022-08-30 21:15:25 +0700

    add wait group

--- Assignment bikin interface Get and Create/main.go
+++ Assignment bikin interface Get and Create/main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+type friend struct {
+	name    string
+	address string
+	job     string
+	reason  string
+}
 
 type userSvc interface {
 	Register(user User) string
@@ -26,6 +34,8 @@ func (u service) Register(user User) string {
 		Nama: user.Nama,
 	})
 
+	fmt.Println(user.Nama + " berhasil didaftarkan")
+
 	return ""
 }
 
@@ -36,17 +46,88 @@ func (u service) GetUser() {
 }
 
 func main() {
+	friends := []friend{
+		{
+			name:    "clara",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "fiqri",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "medy",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "lutfi",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "raiza",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "ian",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "umar",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "fazry",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "pandi",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		}, {
+			name:    "tantut",
+			address: "bekasi",
+			job:     "developer",
+			reason:  "training",
+		},
+	}
+
 	var usernya []User
-	usernya = append(usernya, User{
-		Nama: "adit",
-	})
-	usernya = append(usernya, User{
-		Nama: "ivan",
-	})
-	usernya = append(usernya, User{
-		Nama: "clara",
-	})
+
+	var wg sync.WaitGroup
+
+	for _, v := range friends {
+		wg.Add(1)
+
+		go func(v2 friend) {
+			usernya = append(usernya, User{
+				Nama: v2.name,
+			})
+			fmt.Println(usernya)
+			wg.Done()
+		}(v)
+	}
+
+	wg.Wait()
+
 	var objectService = NewUserSvc(usernya)
 
+	// objectService.Register(User{
+	// 	Nama: "adit",
+	// })
+	// objectService.Register(User{
+	// 	Nama: "ivan",
+	// })
+	// objectService.Register(User{
+	// 	Nama: "clara",
+	// })
 	objectService.GetUser()
 }
